009beda26e6d406e0091168487f66c990ba6e36d
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-04-04 19:34:27 +0100

    Draft Client Class

---
+++ src/Client.cpp
@@ -0,0 +1,59 @@
+#include "Client.hpp"
+
+typedef std::string str;
+
+Client::Client(str nick, int socket_fd) : _nick(nick), _socket_fd(socket_fd)
+{
+    this->_is_admin = false;
+};
+
+Client::Client(str nick, str user, int socket_fd) : _nick(nick), _user(user), _socket_fd(socket_fd)
+{
+    this->_is_admin = false;
+};
+
+Client::~Client(){};
+
+const str &Client::getclientnick() const
+{
+    return this->_nick;
+};
+
+const str &Client::getclientuser() const
+{
+    return this->_user;
+};
+
+const str &Client::getmessage() const
+{
+    return this->_message;
+};
+
+bool Client::is_admin() const
+{
+    return this->_is_admin;
+};
+
+int Client::getclientsocket() const
+{
+    return this->_socket_fd;
+}
+void Client::set_message(str message)
+{
+    this->_message = message;
+}
+
+void Client::set_admin(bool switch_admin)
+{
+    this->_is_admin = switch_admin;
+}
+
+void Client::setnick(str new_nick)
+{
+    this->_nick = new_nick;
+}
+
+void Client::setuser(str new_user)
+{
+    this->_user = new_user;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Server.hpp>
+#include <Client.hpp>
 
 int main(int argc, char **argv)
 {
@@ -8,5 +9,17 @@ int main(int argc, char **argv)
 		std::cout << GREEN "\t->Usage :\"./ircserv <port> <password>\"" BLANK << std::endl;
 		return 1;
 	}
-    Server server(argv[1], argv[2]);
+
+	std::cout << "TESTES" << std::endl;
+	Client joao("Joao", "sou o João", 2);
+	std::cout << joao.getclientuser() << std::endl;
+	joao.set_message("AAA");
+	std::cout << joao.getmessage() << std::endl;
+	std::cout << joao.is_admin() << std::endl;
+	joao.set_admin(true);
+	std::cout << joao.is_admin() << std::endl;
+	std::cout << joao.getclientsocket() << std::endl;
+	std::cout << joao.getclientnick() << std::endl;
+
+	Server server(argv[1], argv[2]);
 }
