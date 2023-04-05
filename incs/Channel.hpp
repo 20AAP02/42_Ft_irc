443dea3fa44a053f0a2cf8756bf252eb3346adbb
António Abranches Pinto <antoniomariabranches@gmail.com>
2023-04-05 12:14:17 +0100

    trying to delete error

--- incs/Channel.hpp
+++ incs/Channel.hpp
@@ -1,18 +1,23 @@
 #ifndef CHANNEL_HPP
 # define CHANNEL_HPP
 
+# include <iostream>
 # include <string>
 # include <vector>
-# include <iostream>
 # include <Client.hpp>
 
 typedef std::string	str;
 
 class Channel
 {
-    public:
-        Channel(const str& name);
-        void addUser(const Client& user);
+
+	public:
+
+		Channel(const str& name);
+		Channel( Channel const & src );
+		~Channel();
+		
+		void addUser(const Client& user);
 
 		// Commands for channel moderators
 		void changeChannelMode(const str& mode);
@@ -20,15 +25,19 @@ class Channel
         void removeUser(const Client& user);
 		void changeTopic(const str& newTopic);
 
+		// Operators
+		Channel &		operator=( Channel const & rhs );
 
-        const std::vector<Client>& getUsers() const;
+		const std::vector<Client>& getUsers() const;
         const str& getName() const;
         const str& getTopic() const;
 
-    private:
-        str _channelName;
+	private:
+		str _channelName;
 		str _channelTopic;
         std::vector<Client> _users;
 };
 
-#endif
+std::ostream &			operator<<( std::ostream & o, Channel const & i );
+
+#endif /* ********************************************************* CHANNEL_H */
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,2 +1,55 @@
 #include <Channel.hpp>
 
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+Channel::Channel(const str& name)
+{
+}
+
+Channel::Channel( const Channel & src ): _channelName(this.)
+{
+}
+
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+Channel::~Channel()
+{
+}
+
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+Channel &				Channel::operator=( Channel const & rhs )
+{
+	//if ( this != &rhs )
+	//{
+		//this->_value = rhs.getValue();
+	//}
+	return *this;
+}
+
+std::ostream &			operator<<( std::ostream & o, Channel const & i )
+{
+	//o << "Value = " << i.getValue();
+	return o;
+}
+
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+
+/* ************************************************************************** */
