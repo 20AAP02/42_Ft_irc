8817fa7bd17b4525592b2f7e7167908c125538ab
Antonio Maria mouro ferreira abranches pinto <antoniomariabranches@gmail.com>
2023-04-19 15:18:48 +0100

    changing way of sending private message

--- src/Client.cpp
+++ src/Client.cpp
@@ -152,6 +152,7 @@ str Client::getNickmask() const {
  void Client::sendPrivateMsg(const Client& recipient, const str& message, const str &type) const
  {
 	std::string msg = ":" + this->getclientnick() + "!~" + this->getNickmask() + " " + type + " " + message + "\n";
+	std::cout << msg;
 	send(recipient.getclientsocket(), msg.c_str(), msg.size(), 0);
  }
 
